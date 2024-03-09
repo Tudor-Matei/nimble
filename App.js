05d327ef56664471a1b39a4c750b8801f1c9a3d4
teoivan <118628182+teoivan@users.noreply.github.com>
2024-03-09 20:17:54 +0200

    navigation

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeRouter, Route, Routes } from 'react-router-native';
 import Login from './Login';
 import Start from './Start';
 import Exercise from './Exercise';
+import Blank from './Blank';
 
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Start />
-      <StatusBar style="auto" />
-    </View>
+    <NativeRouter>
+      <View style={styles.container}>
+        <Routes>
+          <Route path="/" element={<Blank />} />
+          <Route path="/workout" element={<Exercise />} />
+        </Routes>
+      </View>
+    </NativeRouter>
   );
 }
 
--- Footer.js
+++ Footer.js
@@ -1,59 +1,71 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Link } from 'react-router-native';
+
+import Exercise from './Exercise';
+import Start from './Start';
+import Blank from './Blank';
 
 export const Footer = () => {
   const [selectedButton, setSelectedButton] = useState('Stats');
 
   const handleButtonPress = (buttonName) => {
     setSelectedButton(buttonName);
+    if (buttonName === 'Workout') {
+       
+      }
   };
 
   return (
-    <View style={styles.footerContainer}>
-      <TouchableOpacity
-        style={[
-          styles.footerButton,
-          selectedButton === 'Stats' && styles.selectedButton,
-        ]}
-        onPress={() => handleButtonPress('Stats')}
-      >
-        <MaterialCommunityIcons
-          name="chart-box-outline"
-          size={25}
-          color={selectedButton === 'Stats' ? '#fff' : '#5e5b08'}
-        />
-        <Text style={[styles.footerButtonText, { color: selectedButton === 'Stats' ? '#fff' : '#5e5b08' }]}>Stats</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[
-          styles.footerButton,
-          selectedButton === 'Workout' && styles.selectedButton,
-        ]}
-        onPress={() => handleButtonPress('Workout')}
-      >
-        <MaterialCommunityIcons
-          name="dumbbell"
-          size={25}
-          color={selectedButton === 'Workout' ? '#fff' : '#5e5b08'}
-        />
-        <Text style={[styles.footerButtonText, { color: selectedButton === 'Workout' ? '#fff' : '#5e5b08' }]}>Workout</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[
-          styles.footerButton,
-          selectedButton === 'Profile' && styles.selectedButton,
-        ]}
-        onPress={() => handleButtonPress('Profile')}
-      >
-        <MaterialCommunityIcons
-          name="account"
-          size={25}
-          color={selectedButton === 'Profile' ? '#fff' : '#5e5b08'}
-        />
-        <Text style={[styles.footerButtonText, { color: selectedButton === 'Profile' ? '#fff' : '#5e5b08' }]}>Profile</Text>
-      </TouchableOpacity>
-    </View>
+        <View style={styles.footerContainer}>
+            <TouchableOpacity
+              style={[
+                styles.footerButton,
+                selectedButton === 'Stats' && styles.selectedButton,
+              ]}
+              onPress={() => handleButtonPress('Stats')}
+            >
+              <MaterialCommunityIcons
+                name="chart-box-outline"
+                size={25}
+                color={selectedButton === 'Stats' ? '#fff' : '#5e5b08'}
+              />
+              <Text style={[styles.footerButtonText, { color: selectedButton === 'Stats' ? '#fff' : '#5e5b08' }]}>Stats</Text>
+            </TouchableOpacity>
+          
+            <TouchableOpacity
+              style={[
+                styles.footerButton,
+                selectedButton === 'Workout' && styles.selectedButton,
+              ]}
+              onPress={() => handleButtonPress('Workout')}
+            >
+              <MaterialCommunityIcons
+                name="dumbbell"
+                size={25}
+                color={selectedButton === 'Workout' ? '#fff' : '#5e5b08'}
+              />
+              <Link to="/workout"><Text style={[styles.footerButtonText, { color: selectedButton === 'Workout' ? '#fff' : '#5e5b08' }]}>Workout</Text>
+              </Link>
+            </TouchableOpacity>
+          
+            <TouchableOpacity
+              style={[
+                styles.footerButton,
+                selectedButton === 'Profile' && styles.selectedButton,
+              ]}
+              onPress={() => handleButtonPress('Profile')}
+            >
+              <MaterialCommunityIcons
+                name="account"
+                size={25}
+                color={selectedButton === 'Profile' ? '#fff' : '#5e5b08'}
+              />
+              <Text style={[styles.footerButtonText, { color: selectedButton === 'Profile' ? '#fff' : '#5e5b08' }]}>Profile</Text>
+            </TouchableOpacity>
+        </View>
+  
   );
 };
 
@@ -67,17 +79,17 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     borderColor: '#e3d87e',
     width: '90%',
-    borderRadius:35,
+    borderRadius: 35,
     overflow: 'hidden',
     marginBottom: 15,
-    alignSelf:'center',
+    alignSelf: 'center',
   },
   footerButton: {
     alignItems: 'center',
     justifyContent: 'center',
     padding: 3,
     borderRadius: 10,
-    width: 55
+    width: 55,
   },
   footerButtonText: {
     textAlign: 'center',
@@ -86,6 +98,6 @@ const styles = StyleSheet.create({
     color: '#5e5b08',
   },
   selectedButton: {
-    backgroundColor: '#5e5b08', 
+    backgroundColor: '#5e5b08',
   },
 });
